0f647b1942a3f44e5bf62db829e90546bf527fbe
T4CEE <cyriliniekong@gmail.com>
2024-10-30 12:58:22 +0100

    domain update

--- app/contact/page.js
+++ app/contact/page.js
@@ -22,13 +22,16 @@ const Contact = () => {
     };
 
     try {
-      const response = await fetch("http://in-motion-web/api/send-email", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
+      const response = await fetch(
+        "http://in-motion-web.vercel.app/api/send-email",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
 
       const result = await response.json();
 
